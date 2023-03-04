04c51e1961d09256025c462e3c8962d61ee75c78
Belbachir Chaimaa <chaimaa.482bel@gmail.com>
2023-03-05 00:35:27 +0100

    commit all

---
+++ src/components/Billing.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles, {layout} from '../style'
+import { apple, bill, google } from '../assets'
+
+const Billing = () => {
+  return (
+    <section id='products' className={layout.sectionReverse}>
+      <div className={layout}>
+
+      </div>
+
+    </section>
+  )
+}
+
+export default Billing
---
+++ src/components/CradDeal.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CradDeal = () => {
+  return (
+    <div>CradDeal</div>
+  )
+}
+
+export default CradDeal
