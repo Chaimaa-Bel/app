cb0c560c1c93e823c1a09775675fbb75248a15b9
Belbachir Chaimaa <chaimaa.482bel@gmail.com>
2023-03-05 01:58:16 +0100

    hey

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,9 @@ const App = () => (
         <Testimonials />
         <Clients />
         <CTA />
-        <Footer />        
+        <Footer />      
+        <Footer />   
+           
       </div>
     </div>
 
